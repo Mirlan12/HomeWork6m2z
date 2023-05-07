c171c60c87e82abda8419cf9907502dd4bba16b1
Mirlan12 <mirlan12991@gamil.com>
2023-05-07 18:53:07 +0600

    Initial commit

---
+++ app/src/main/java/com/example/HomeWork6m2z/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.HomeWork6m2z
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.HomeWork6m2z.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var selectedAdapter: SelectedAdapter
+    private var selectedImage = arrayListOf<ImageModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getData()
+        initViews()
+    }
+
+    private fun getData() {
+        selectedImage = intent.getParcelableArrayListExtra(EditActivity.AppConstant.SELECTED_LIST)!!
+    }
+
+    private fun initViews() {
+        selectedAdapter = SelectedAdapter(selectedImage)
+        binding.rvMain.layoutManager = GridLayoutManager(this, 3)
+        binding.rvMain.adapter = selectedAdapter
+    }
+}
